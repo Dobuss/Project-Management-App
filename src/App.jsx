35604ff916448a6f88bd79483fdaa3ed6e40ba5a
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-06 16:19:30 +0200

    impl fallback component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <main className="h-screen my-8 flex gap-8">
       <Sidebar/>
       <NewProject/>
+      <NoProjectSelected/>
     </main>
   );
 }

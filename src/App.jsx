36f4ab34dd0662d6ee9fdcd293fe32dedc2e22b7
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-06 16:30:53 +0200

    button to switch bw components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import Sidebar from "./components/Sidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject(){
+    setProjectsState(prev => {
+      return {
+        ...prev,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <Sidebar/>
-      <NewProject/>
-      <NoProjectSelected/>
+      <Sidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </main>
   );
 }
--- src/components/NoProjectSelected.jsx
+++ src/components/NoProjectSelected.jsx
@@ -1,14 +1,14 @@
 import noProjects from '../assets/no-projects.png'
 import Button from './Button';
 
-export default function NoProjectSelected() {
+export default function NoProjectSelected({onStartAddProject}) {
   return (
     <div className="mt-24 text-center w-2/3">
       <img src={noProjects} alt="empty task" className='w-16 h-16 object-contain mx-auto'/>
       <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
       <p className='text-stone-400 mb-4'>Select a project or get started with a new one!</p>
       <p className='mt-8'>
-        <Button>Create new project</Button>
+        <Button onClick={onStartAddProject}>Create new project</Button>
       </p>
     </div>
   );

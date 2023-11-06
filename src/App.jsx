a9da919ff59a18c65f3cac800aa8f7303df42833
Dobuss <dobromira.ivanova911@gmail.com>
2023-11-06 15:15:51 +0200

    impl basic layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./components/NewProject";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <Sidebar/>
+      <NewProject/>
     </main>
   );
 }
